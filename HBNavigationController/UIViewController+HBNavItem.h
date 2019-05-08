03e85ad26ccb8ef7bc46d5c6c0f377d0febcfea5
Patrick.W <cheyun@cheyundeiMac.local>
2019-05-08 15:32:55 +0800

    navigationItem

--- HBNavigationController/UIViewController+HBNavItem.h
+++ HBNavigationController/UIViewController+HBNavItem.h
@@ -12,8 +12,41 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIViewController (HBNavItem)
 
+/**
+ * 快速创建LeftBarButtonItem
+ * @param image 图片
+ */
+- (void)setLeftBarButtonItemImage:(UIImage *)image;
 
+/**
+ * 快速创建RightBarButtonItem
+ * @param image 图片
+ */
+- (void)setRightBarButtonItemImage:(UIImage *)image;
 
+/**
+ * 快速创建LeftBarButtonItem
+ * @param title 标题
+ * @param color 颜色
+ * @param font 字体
+ */
+- (void)setLeftBarButtonItemTitle:(NSString *)title andColor:(UIColor *)color font:(UIFont *)font;
+
+/**
+ * 快速创建RightBarButtonItem
+ * @param title 标题
+ * @param color 颜色
+ * @param font 字体
+ */
+- (void)setRightBarButtonItemTitle:(NSString *)title andColor:(UIColor *)color font:(UIFont *)font;
+
+// LeftBarButtonItem被点击，可复写此方法
+// 默认为返回操作
+- (void)leftBarButtonItemDidClicked:(nullable id)sender;
+
+// RightBarButtonItem被点击，可复写此方法
+// do nothing
+- (void)rightBarButtonItemDidClicked:(nullable id)sender;
 
 @end
 
--- HBNavigationController/UIViewController+HBNavItem.m
+++ HBNavigationController/UIViewController+HBNavItem.m
@@ -10,4 +10,63 @@
 
 @implementation UIViewController (HBNavItem)
 
+- (UIBarButtonItem *)barButtonItemWithImage:(UIImage *)image action:(SEL)action {
+    UIImage *img = [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithImage:img style:UIBarButtonItemStylePlain target:self action:action];
+    return item;
+}
+
+- (UIBarButtonItem *)barButtonItemWithTitle:(NSString *)title andColor:(UIColor *)color font:(UIFont *)font action:(SEL)action {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:title forState:UIControlStateNormal];
+    [button setTitleColor:color forState:UIControlStateNormal];
+    [button.titleLabel setFont:font];
+    [button addTarget:self action:action forControlEvents:UIControlEventTouchUpInside];
+    [button sizeToFit];
+    button.frame = CGRectMake(0, 0, MAX(44, button.frame.size.width), 44);
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithCustomView:button];
+    return item;
+}
+
+- (void)setLeftBarButtonItemImage:(UIImage *)image {
+    SEL action = @selector(leftBarButtonItemDidClicked:);
+    UIBarButtonItem *leftItem = [self barButtonItemWithImage:image action:action];
+    self.navigationItem.leftBarButtonItem = leftItem;
+}
+
+- (void)setRightBarButtonItemImage:(UIImage *)image {
+    SEL action = @selector(rightBarButtonItemDidClicked:);
+    UIBarButtonItem *rightItem = [self barButtonItemWithImage:image action:action];
+    self.navigationItem.rightBarButtonItem = rightItem;
+}
+
+- (void)setLeftBarButtonItemTitle:(NSString *)title andColor:(UIColor *)color font:(UIFont *)font {
+    SEL action = @selector(leftBarButtonItemDidClicked:);
+    UIBarButtonItem *leftItem = [self barButtonItemWithTitle:title
+                                                    andColor:color
+                                                        font:font
+                                                      action:action];
+    self.navigationItem.leftBarButtonItem = leftItem;
+}
+
+- (void)setRightBarButtonItemTitle:(NSString *)title andColor:(UIColor *)color font:(UIFont *)font {
+    SEL action = @selector(rightBarButtonItemDidClicked:);
+    UIBarButtonItem *rightItem = [self barButtonItemWithTitle:title
+                                                     andColor:color
+                                                         font:font
+                                                       action:action];
+    self.navigationItem.rightBarButtonItem = rightItem;
+}
+
+- (void)leftBarButtonItemDidClicked:(nullable id)sender {
+    UIViewController *popViewController = [self.navigationController popViewControllerAnimated:YES];
+    if (!popViewController && self.presentingViewController) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+
+- (void)rightBarButtonItemDidClicked:(nullable id)sender {
+    
+}
+
 @end
